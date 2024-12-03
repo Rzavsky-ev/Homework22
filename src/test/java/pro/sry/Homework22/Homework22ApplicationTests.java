77636b7772c79ff0af856a531f95dd0f0ac93c98
Rzavsky-ev <rzavsky.ev@gmail.com>
2024-12-03 17:34:06 +0300

    Первый коммит

---
+++ src/test/java/pro/sry/Homework22/Homework22ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sry.Homework22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
